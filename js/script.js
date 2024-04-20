18d0835812766ccf47aece38adbfa8b9d76158f1
ardhikaxx <ardhikayanuar58@gmail.com>
2024-04-20 18:06:45 +0700

    update

--- js/script.js
+++ js/script.js
@@ -123,3 +123,21 @@ document.getElementById('kirim').addEventListener('click', function () {
         sendWhatsAppMessage(nama, pesan);
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    function handleMenuClick(event) {
+        event.preventDefault();
+
+        var targetId = this.getAttribute('href').substring(1);
+
+        var targetElement = document.getElementById(targetId);
+
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    var menuLinks = document.querySelectorAll('.nav-link');
+
+    menuLinks.forEach(function(link) {
+        link.addEventListener('click', handleMenuClick);
+    });
+});
